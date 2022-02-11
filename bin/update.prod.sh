2d500532231753ca3fe21bf2f571e5f0df221de3
Gajo Petrovic <gajopetrovic@gmail.com>
2022-02-11 16:39:00 +0900

    Try to fix update prod script: use absolute paths

--- bin/update.prod.sh
+++ bin/update.prod.sh
@@ -5,12 +5,12 @@ export SLEEP_TIME=60
 echo "Starting updater script..."
 
 echo "Updating rapid(registry)..."
-./sprd --root-folder /rapid download-registry
+/app/sprd --root-folder /rapid download-registry
 echo "Updating rapid(repo)..."
-./sprd --root-folder /rapid download-repo
+/app/sprd --root-folder /rapid download-repo
 
 echo "Updating database..."
-./update --root-folder /rapid
+/app/update --root-folder /rapid
 echo "Done. Sleeping for ${SLEEP_TIME} seconds..."
 
 sleep $SLEEP_TIME
